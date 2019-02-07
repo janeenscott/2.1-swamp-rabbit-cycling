125affc437df533df1434e4807546e0b221f31f1
Janeen Scott <janeen.e.scott@gmail.com>
2019-02-06 19:20:00 -0500

    installed json, getting errors

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import request
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -13,23 +14,21 @@ def index():
     response = requests.get('https://api.etsy.com/v2/listings/active.?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score')
     data = response.json()
 
-    print(data[results])
+    print(data)
 
     bicycles = data['results']
 
     etsy_data = []
 
-
-
     for listing in bicycles:
-        etsy_data.append(''.format(listing['name'],))
+        etsy_data.append('<li>{}</li>'.format(listing['name'],))
 
     etsy_data = ''.join(etsy_data)
 
     # Get html and render to screen
     index_file = open('index.html', 'r')
     index_html = index_file.read()
-    index_html = index_html.replace('{{bikes}}', planet_html)
+    index_html = index_html.replace('{{bikes}}', etsy_data)
     index_file.close()
 
     return index_html
