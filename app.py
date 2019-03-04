a672a202964f7452dde9a08bf617f5bce14de942
Janeen <janeen.e.scott@gmail.com>
2019-03-04 15:52:42 -0500

    solved internal server error

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    response = requests.get('https://api.etsy.com/v2/listings/active.?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score')
+    response = requests.get('https://api.etsy.com/v2/listings/active?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score')
     data = response.json()
 
     print(data)
@@ -21,14 +21,15 @@ def index():
     etsy_data = []
 
     for listing in bicycles:
-        etsy_data.append('<li>{}</li>'.format(listing['name'],))
+        # etsy_data.append('<li>{}</li>'.format(listing['name'],))
+        print(listing['Images'])
 
-    etsy_data = ''.join(etsy_data)
+    # etsy_data = ''.join(etsy_data)
 
     # Get html and render to screen
     index_file = open('index.html', 'r')
     index_html = index_file.read()
-    index_html = index_html.replace('{{bikes}}', etsy_data)
+    # index_html = index_html.replace('{{bikes}}', etsy_data)
     index_file.close()
 
     return index_html
